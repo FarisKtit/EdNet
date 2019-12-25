cccbf309bfe06e8f8b0710ba710695a0309e619b
Faris Ktit <farisktit@hotmail.co.uk>
2019-12-25 15:29:33 +0000

    Basic edit page created, lack of profile image editability

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Occupation;
 
 class ProfileController extends Controller
 {
@@ -20,5 +22,25 @@ class ProfileController extends Controller
     {
       $user = Auth::user();
       return view('dashboard.profile.user_profile', compact('user'));
+    }
+
+    public function edit_profile()
+    {
+      $user = Auth::user();
+      $occupations = Occupation::all();
+      return view('dashboard.profile.user_profile_edit', compact('user', 'occupations'));
+    }
+
+    public function update_profile(Request $request)
+    {
+      $user = Auth::user();
+      $user->name = $request->name;
+      $user->birthdate = $request->birthdate;
+      $user->about = $request->about;
+      $user->save();
+      $occupation_id = $request->occupation;
+      $user_id = $user->id;
+      DB::update('UPDATE occupations_users SET occupation_id = ? WHERE user_id = ?', [$occupation_id, $user_id]);
+      return redirect()->back()->with('success', 'Successfully updated your profile information.');
     }
 }
