d55e38b95039f4d709eef99af39d0b83bf5d1d1e
Faris Ktit <farisktit@hotmail.co.uk>
2019-12-29 01:52:14 +0000

    Ability to view other users with a relationship is implemented

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Occupation;
 use App\Post;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -22,13 +23,14 @@ class ProfileController extends Controller
     public function index()
     {
       $user = Auth::user();
+      $user_id = $user->id;
       $posts = $user->posts;
       $posts = $posts->sortByDesc('id');
 
-      $occupation = DB::select("SELECT o.name FROM occupations AS o INNER JOIN users
-      AS u ON o.id = u.occupation_id WHERE u.id = ?", [Auth::user()->id]);
-      $occupation = $occupation[0]->name;
-      return view('dashboard.profile.user_profile', compact('user', 'posts', 'occupation'));
+      $user = DB::select("SELECT u.name, u.birthdate, u.about, u.profile_image_filename, o.name AS 'occupation' FROM users AS u
+      INNER JOIN occupations AS o ON u.occupation_id = o.id WHERE u.id = ?", [$user_id]);
+      $user = $user[0];
+      return view('dashboard.profile.user_profile', compact('user', 'posts'));
     }
 
     public function edit_profile()
@@ -48,5 +50,36 @@ class ProfileController extends Controller
       $user->save();
 
       return redirect()->back()->with('success', 'Successfully updated your profile information.');
+    }
+
+    /**
+     * Visit other user profiles.
+     *
+     *
+     */
+    public function view_user_profile($id)
+    {
+        $visited_id = $id;
+        $visitor_id = Auth::user()->id;
+
+        $user = User::findOrFail($id);
+        $posts = $user->posts;
+        $posts = $posts->sortByDesc('id');
+
+        $res = DB::select("SELECT id FROM relationships_users WHERE ((requester_id = ? AND responder_id = ?) OR (requester_id = ? AND responder_id = ?)) AND accepted = 1",
+        [$visited_id, $visitor_id, $visitor_id, $visited_id]);
+
+        $user = DB::select("SELECT u.name, u.birthdate, u.about, u.profile_image_filename, o.name AS 'occupation' FROM users AS u
+        INNER JOIN occupations AS o ON u.occupation_id = o.id WHERE u.id = ?", [$visited_id]);
+        $user = $user[0];
+
+        if(count($res) == 0) {
+          return view('dashboard.profile.user_profile_relationship_not_formed', compact('user'));
+        }
+
+
+
+        return view('dashboard.profile.user_profile_relationship_formed', compact('user', 'posts'));
+
     }
 }
