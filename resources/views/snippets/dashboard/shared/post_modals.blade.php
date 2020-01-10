7fb385ebeaaf74b3a63ebde2fd0a648445e9a6aa
Faris Ktit <farisktit@hotmail.co.uk>
2020-01-10 00:22:50 +0000

    Home wall basic functionality complete

---
+++ resources/views/snippets/dashboard/shared/post_modals.blade.php
@@ -0,0 +1,202 @@
+<div id="add_comment_to_post_modal" class="add_comment_to_post_modal">
+
+  <div class="add_comment_to_post_modal-content">
+
+    <div class="row add-comment-to-post-form">
+
+      <div class="col-md-12">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <h5>Add comment below.</h5>
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-3">
+
+          </div>
+
+          <div class="col-md-6">
+
+            <div class="form-group">
+
+              <textarea class="form-control" id="add_comment_to_post_content" rows="3"></textarea>
+
+            </div>
+
+
+          </div>
+
+          <div class="col-md-3">
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" class="btn btn-sm btn-default" id="add_comment_to_post_submit_btn">Add Comment</button>
+
+          </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" class="btn btn-sm btn-danger cancel-add-comment-screen-btn">Cancel</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="row add-comment-to-post-gif" >
+
+      <div class="col-md-12">
+
+        <img src="/img/ajax_loader.gif" class="">
+
+        <h4>Adding comment to post..</h4>
+
+      </div>
+
+    </div>
+
+    <div class="row add-comment-to-post-complete-screen" >
+
+      <div class="col-md-12">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <h5 class="add-comment-to-post-complete-msg"></h5>
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" class="btn btn-sm btn-default add-comment-to-post-complete-screen-close-btn">Close Window</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<div id="delete_comment_from_post_modal" class="delete_comment_from_post_modal">
+
+  <div class="delete_comment_from_post_modal-content">
+
+    <div class="row delete-comment-from-post-prompt">
+
+      <div class="col-md-12">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <h5>Are you sure you want to delete this comment?</h5>
+
+          </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" data-comment="" data-post="" class="btn btn-sm btn-success" id="delete_comment_from_post_submit_btn">Yes, delete my comment</button>
+
+          </div>
+
+
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" class="btn btn-sm btn-danger cancel-delete-comment-screen-btn">No, thanks</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="row delete-comment-from-post-gif" >
+
+      <div class="col-md-12">
+
+        <img src="/img/ajax_loader.gif" class="">
+
+        <h4>Deleting comment from post..</h4>
+
+      </div>
+
+    </div>
+
+    <div class="row delete-comment-from-post-complete-screen" >
+
+      <div class="col-md-12">
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <h5 class="delete-comment-from-post-complete-msg"></h5>
+
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <button type="button" name="button" class="btn btn-sm btn-default delete-comment-from-post-complete-screen-close-btn">Close Window</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
